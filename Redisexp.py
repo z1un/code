da96596518b995b782350d7ee27c8afd0a74e7a6
z1un <z1un@qq.com>
2020-09-24 08:48:23 +0800

    upload

--- Redisexp.py
+++ Redisexp.py
@@ -1,7 +1,12 @@
 # _*_  coding:utf-8 _*_
 import socket
 import sys
-PASSWORD_DIC=['redis','root','oracle','password','p@aaw0rd','abc123!','123456','admin']
+PASSWORD_DIC = [
+    'redis', 'root', 'oracle', 'password', 'p@aaw0rd', 'abc123!', '123456',
+    'admin', '123'
+]
+
+
 def check(ip, port, timeout):
     try:
         socket.setdefaulttimeout(timeout)
@@ -15,13 +20,16 @@ def check(ip, port, timeout):
             for pass_ in PASSWORD_DIC:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect((ip, int(port)))
-                s.send("AUTH %s\r\n" %(pass_))
+                s.send("AUTH %s\r\n" % (pass_))
                 result = s.recv(1024)
                 if '+OK' in result:
                     return u"存在弱口令，密码：%s" % (pass_)
     except Exception, e:
         pass
+
+
 if __name__ == '__main__':
-    ip=sys.argv[1]
-    port=sys.argv[2]
-    print check(ip,port, timeout=10)
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    print check(ip, port, timeout=10)
+
